b45a77c77dfab528cffc26c8ad81b002330973f0
Lina <61861965+l1na-forever@users.noreply.github.com>
2023-08-24 19:56:30 -0700

    first working example tested e2e

--- create.sh
+++ create.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-DOMAIN_NAME=$1
-STACK_NAME=$(echo "$DOMAIN_NAME" | cut -f 1 -d '.')
+set -e
 
-if [ -z "$DOMAIN_NAME" ]; then
-  echo "Usage: ./create.sh example.com"
+DOMAIN_NAME="$1"
+CERTIFICATE_ARN="$2"
+STACK_NAME=$(echo "www-$DOMAIN_NAME" | cut -f 1 -d '.')
+
+if [[ -z "$DOMAIN_NAME" || -z "$CERTIFICATE_ARN" ]]; then
+  echo "Usage: ./create.sh example.com arn:aws:us-east-1:acm_cert"
   exit 1
 fi
 
-aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body file://static_cloudfront_site.yml --parameters ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME
+aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body file://static_cloudfront_site.yml --parameters ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME ParameterKey=DomainCertificateArn,ParameterValue=$CERTIFICATE_ARN
